d9da316b6dacda08b4df3b3a960f79d830b65e5b
Menma736 <155307268+Menma736@users.noreply.github.com>
2026-06-21 08:20:57 -0400

    Update create_image.sh to handle Arch Linux image labels and distro names

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -23,46 +23,6 @@ status_green() { #announce the success of a major action
   echo -e "\e[92m$1\e[0m" 1>&2
 }
 
-# Detect distro and set appropriate commands
-detect_distro() {
-  if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    echo "$ID"
-  else
-    echo "unknown"
-  fi
-}
-
-CURRENT_DISTRO=$(detect_distro)
-status "Detected distro: $CURRENT_DISTRO"
-
-# Set distro-specific commands
-case "$CURRENT_DISTRO" in
-  arch|artix)
-    ZEROFREE_CMD="zerofree"
-    E2LABEL_CMD="e2label"
-    MKFS_CMD="mkfs.ext4"
-    ;;
-  ubuntu|debian)
-    ZEROFREE_CMD="zerofree"
-    E2LABEL_CMD="e2label"
-    MKFS_CMD="mkfs.ext4"
-    ;;
-  *)
-    warning "Unknown distro, assuming standard Linux tools available"
-    ZEROFREE_CMD="zerofree"
-    E2LABEL_CMD="e2label"
-    MKFS_CMD="mkfs.ext4"
-    ;;
-esac
-
-# Check for required commands
-for cmd in "$MKFS_CMD" "$E2LABEL_CMD" "$ZEROFREE_CMD" "7z" "split" "dd" "mount" "umount"; do
-  if ! command -v "$cmd" &> /dev/null; then
-    error "Required command not found: $cmd"
-  fi
-done
-
 # move to current script directory regardless of where the script was run from
 cd `dirname $0` || exit 1
 cd ../output || error "Failed to move to output directory"
@@ -74,13 +34,16 @@ dd if=/dev/zero of=l4t.ext4.img bs=4194304 count=$(($bytes / 4194304 + 200))
 # dd if=/dev/zero of=l4t.ext4.img bs=4194304 count=1950
 status "Formatting to ext4"
 sync
-"$MKFS_CMD" -b 4096 l4t.ext4.img
+mkfs.ext4 -b 4096 l4t.ext4.img
 case "$1" in
 *-jammy)
-"$E2LABEL_CMD" l4t.ext4.img SWR-JAM
+e2label l4t.ext4.img SWR-JAM
 ;;
 *-noble)
-"$E2LABEL_CMD" l4t.ext4.img SWR-NOB
+e2label l4t.ext4.img SWR-NOB
+;;
+arch)
+e2label l4t.ext4.img SWR-ARCH
 ;;
 esac
 mkdir -p mounted_ext4
@@ -94,14 +57,20 @@ cp -a rootfs/* mounted_ext4/ || error "Failed to copy rootfs to mount"
 sync
 umount mounted_ext4
 status "Cleaning up free space"
-"$ZEROFREE_CMD" l4t.ext4.img
+zerofree l4t.ext4.img
 
 status "Spliting image"
 split -b4290772992 --numeric-suffixes=0 "l4t.ext4.img" "l4t."
 #rm -f l4t.ext4.img
 sync
 
-distro_name=$(echo "$1" | awk -F- '{print $NF}')
+# Handle distro name for arch
+if [[ "$1" == "arch" ]]; then
+  distro_name="arch"
+else
+  distro_name=$(echo "$1" | awk -F- '{print $NF}')
+fi
+
 status "Creating L4T-$1-image.7z"
 rm -rf L4T-image
 sudo -u "$SUDO_USER" mkdir L4T-image
